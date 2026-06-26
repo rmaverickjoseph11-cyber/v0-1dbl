484fe71ed320e6637bb06bfe2ea6a16dc501d39e
richardjoseph-ux <richard.joseph@phaetontrucking.com>
2026-06-26 12:23:16 +0800

    tab update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: '1 Day Basketball League',
+  title: '',
   description: 'Register for the 1 Day Basketball League',
   generator: 'v0.app',
   icons: {
